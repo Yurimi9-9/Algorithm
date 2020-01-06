d9ddc23feaa1452bc213e816b7546998f7397562
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-01-07 02:51:47 +0900

    redraw BaekJoon_2606.cpp
    DFS

--- Search/Graph(DFS, BFS)/BaekJoon_2606_DFS.cpp
+++ Search/Graph(DFS, BFS)/BaekJoon_2606_DFS.cpp
@@ -1,18 +1,17 @@
-// 백준 1260 - DFS와 BFS
+// 백준 2606 - 바이러스 (DFS 탐색)
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int dfs(vector<vector<int> > &graph, bool visited[], int V, int count);
+int infected=0;	// 1번 컴퓨터에 의해 감염된 컴퓨터의 수 카운트
+
+void dfs(vector<vector<int> > &graph, bool visited[], int V);
 
 int main()
 {
 	int N=0, M=0;	// N: 정점의 개수, M: 간선의 개수
 	cin >> N >> M;
-	int count=0;
 
 	vector<vector<int> > graph(N+1);
 	bool visited[N+1]={};	// 각 노드들의 방문 상태
@@ -25,22 +24,23 @@ int main()
 		graph[b].push_back(a);	// 그래프 간선 연결
 	}
 
-	count = dfs(graph, visited, 1, count);
+	dfs(graph, visited, 1);
 
-	cout << count;
+	cout << infected;
 
 	return 0;
 }
 
 
-int dfs(vector<vector<int> > &graph, bool visited[], int V, int count)
+void dfs(vector<vector<int> > &graph, bool visited[], int V)
 {
 	visited[V] = true;
 
 	vector<int>::iterator itr = graph[V].begin();
 	for(; itr!=graph[V].end(); itr++)
-	{
 		if(!visited[*itr])
-			count = dfs(graph, visited, *itr, count)+1;
-	}
+		{
+			dfs(graph, visited, *itr);
+			infected++;		// dfs 탐색으로 노드를 하나씩 찾아갈 때마다 카운트 증가
+		}
 }
