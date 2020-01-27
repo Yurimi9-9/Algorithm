d1b5af22a3cd73b4500fcea10f38754bfbf7367f
Yurimi <59168302+Yurimi9-9@users.noreply.github.com>
2020-01-28 03:02:52 +0900

    Create merge_sort.cpp

---
+++ Sort/merge_sort.cpp
@@ -0,0 +1,38 @@
+// 정렬 - 병합 정렬
+void merge(int a[], int first, int mid, int last)
+{
+	int temp[last-first +1]={};
+	int i, j, k;
+	i = first;
+	j = mid +1;
+	k = 0;
+
+	while(i<=mid && j<=last)
+		if(a[i] <= a[j])
+			temp[k++] = a[i++];
+		else
+			temp[k++] = a[j++];
+
+	if(i<=mid)
+		for( ; i<=mid; i++)
+			temp[k++] = a[i];
+	else
+		for( ; j<=last; j++)
+			temp[k++] = a[j];
+
+	for(i=first; i<=last; i++)
+		a[i] = temp[i-first];
+}
+
+void mergeSort(int a[], int first, int last)
+{
+	if(first < last)
+	{
+		int mid = (first+last)/2;
+		mergeSort(a, first, mid);		// 1. 분할	: n/2로 나눔
+		mergeSort(a, mid+1, last);		// 	      	: O(log n)
+		merge(a, first, mid, last);		// 2. 정복	: 반으로 나눈 데이터 합치기
+										//			: n만큼 비교, O(n)
+	}
+}
+// 시간 복잡도 = O(n log n)		분할: O(log n) + 정복: O(n)
